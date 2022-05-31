0698a9df3c9483d241845b3cf5d37500f0dfd48c
Ray <qianlei6148@163.com>
2022-05-31 10:15:19 +0800

    fix(qian):vuex添加tabbar参数

--- store/index.js
+++ store/index.js
@@ -1,16 +1,20 @@
 import user from '@/store/modules/user.js'
 import vuexMusic from '@/store/modules/music.js'
+import tabbar from '@/store/modules/tabbar.js'
 
 // #ifndef VUE3
 import Vue from 'vue'
 import Vuex from 'vuex'
 Vue.use(Vuex)
+// const Logger = Vuex.createLogger()
 const store = new Vuex.Store({
 	modules: {
 		user,
-		vuexMusic
+		vuexMusic,
+		tabbar
 	},
 	strict: true
+	// plugins: debug ? [Logger()] : []
 })
 // #endif
 
@@ -19,8 +23,11 @@ import {createStore} from 'vuex'
 const store = createStore({
 	modules: {
 		user,
-		vuexMusic
+		vuexMusic,
+		tabbar
 	}
+	// strict: debug,
+	// plugins: debug ? [Logger()] : []
 })
 // #endif
 
